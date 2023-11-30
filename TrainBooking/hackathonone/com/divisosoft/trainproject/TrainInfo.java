a16797f89a95c29ae44ef6cba967e05e72aad6ac
mageshlxi <magesh.m.e@lxisoft.com>
2023-11-30 11:47:01 +0530

    instance variables created

---
+++ TrainBooking/hackathonone/com/divisosoft/trainproject/TrainInfo.java
@@ -0,0 +1,40 @@
+public class TrainInfo{
+	
+private String name;
+private int number;
+private String fromLocation;
+private String destination;
+
+	public void setName(String name){
+		this.name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setNumber(String number){
+		this.number;
+	}
+	
+	public String getNumber(){
+		return number;
+	}
+	
+	public void setFromLocation(String FromLocation){
+		this.FromLocation;
+	}
+	
+	public String getFromLocation(){
+		return FromLocation;
+	}
+
+	public void setDestination(String destination){
+		this.destination;
+	}
+	
+	public String getDestination(){
+		return destination;
+	}	
+	
+}
